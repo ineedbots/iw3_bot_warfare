01917f1661565fa23193653796638c19ccdd976e
INeedBots <ineedbots@outlook.com>
2020-12-17 02:38:48 -0600

    need to be modded to have custom maps

--- z_server.bat
+++ z_server.bat
@@ -1,1 +1,1 @@
-start "" "%~dp0cod4x18_dedrun.exe" +set dedicated "2" +set sv_maxclients "64" +set sv_punkbuster "0" +set net_port "28960" +set fs_game "" +exec server.cfg +loadplugin httpget +map_rotate
+start "" "%~dp0cod4x18_dedrun.exe" +set dedicated "2" +set sv_maxclients "64" +set sv_punkbuster "0" +set net_port "28960" +set fs_game "mods/bots" +exec server.cfg +loadplugin httpget +map_rotate
