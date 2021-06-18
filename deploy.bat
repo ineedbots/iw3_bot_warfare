6d9dbb262a4a71b7d8c237c5908f67d26640730d
ineedbots <ineedbots@outlook.com>
2021-06-18 14:17:58 -0600

    Added deploy and astylerc

---
+++ deploy.bat
@@ -0,0 +1,4 @@
+xcopy cod4x_bot_warfare\main_shared\maps\mp\bots main_shared\maps\mp\bots\ /Y /I /E /H /C
+xcopy cod4x_bot_warfare\main\waypoints main\waypoints\ /Y /I /E /H /C
+xcopy cod4x_bot_warfare\main\botnames.txt main\botnames.txt /Y /I /H /C
+xcopy cod4x_bot_warfare\main\plugins\httpget.dll main\plugins\httpget.dll /Y /I /H /C
