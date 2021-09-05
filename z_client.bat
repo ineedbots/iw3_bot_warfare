b5fb8c776053be178b8d5ed39075b25d9a9038ff
ineedbots <ineedbots@outlook.com>
2021-09-04 23:59:17 -0600

    clean up

--- z_client.bat
+++
@@ -1,1 +0,0 @@
-start "" "%~dp0iw3mp.exe" +set r_fullscreen "1" +set cg_drawlagometer "0" +set cg_drawfps "0" +set cg_drawsnapshot "0" +set r_mode "1920x1080" +set r_monitor "0"
--- z_devserver.bat
+++
@@ -1,1 +0,0 @@
-start "" "%~dp0iw3mp.exe" +set r_fullscreen "0" +set cg_drawlagometer "1" +set developer "1" +set developer_script "1" +set logfile "2" +set cg_drawfps "3" +set cg_drawsnapshot "1" +set scr_game_spectatetype "2" +set r_mode "1024x768" +set thereisacow "1" +set fs_game "mods/dev" +set sv_cheats "1" +connect 127.0.0.1
