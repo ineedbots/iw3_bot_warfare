abfa41e24df3e2be70944f1530ac7f505533b128
ineed bots <ineedbots@pbot.org>
2022-05-08 18:01:17 -0600

    smaall changes

--- out/Add to root of CoD4x server/z_localserver.bat
+++ out/Add to root of CoD4x server/z_localserver.bat
@@ -1,1 +1,1 @@
-start "" "%~dp0cod4x18_dedrun.exe" +set dedicated "1" +set sv_maxclients "64" +set sv_punkbuster "0" +set net_port "28965" +set fs_game "mods/bots" +exec localbotserver.cfg +loadplugin httpget +map_rotate
+start "" "%~dp0cod4x18_dedrun.exe" +set dedicated "1" +set sv_maxclients "64" +set net_port "28965" +set fs_game "mods/bots" +exec localbotserver.cfg +map_rotate
